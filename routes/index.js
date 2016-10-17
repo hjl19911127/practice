65346a82e89c282363cd548c2bcca7c088bf5a79
suj <suj@101.com>
2016-10-17 10:43:02 +0800

    chapter23.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res) {
-	res.render('index', {title: 'JavaScript高级程序设计'});
+	res.render('chapter23', {title: 'JavaScript高级程序设计'});
+});
+
+router.get('/chapter23', function(req, res) {
+	res.render('chapter23', {title: 'JavaScript高级程序设计'});
 });
 
 module.exports = router;
